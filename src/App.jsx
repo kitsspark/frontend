e6b6cb3d60e303d959e808baba31c47a7de40d1b
sivaramireddy <baramsivarami@gmail.com>
2023-08-27 11:54:07 +0530

    change file name

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import { useState } from 'react'
 import { RouterProvider, Route,  createRoutesFromElements,createBrowserRouter } from 'react-router-dom'
-import Layout from './layout'
-import Errorpage from './pages/Errorpage'
-import Homepage from './pages/Homepage'
-import Notes from './pages/Notes'
-import Timetable from './pages/Timetable'
-import Login from './pages/Login'
-
+import Layout from './layout.jsx'
+import Errorpage from './pages/Errorpage.jsx'
+import Homepage from './pages/Homepage.jsx'
+import Notes from './pages/Notes.jsx'
+import Timetable from './pages/Timetable.jsx'
+import Login from './pages/Login.jsx'
+import Dashboard from './pages/Dashboard.jsx'
 // actions
 
-import { loginAction } from './pages/Login'
-import Dashboard from './pages/Dashboard'
+import { loginAction } from './pages/Login.jsx'
+
 function App() {
 
 
